35755164e5c014d7e72e6bf7228a97136906bb66
nehasha001 <neha.shaik777@gmail.com>
2019-01-09 02:31:47 -0500

    Initial commit

---
+++ JSon.java
@@ -0,0 +1,20 @@
+/*
+package Json;
+import org.json.simple.JSONObject;
+
+import java.io.StringWriter;
+
+class JSon {
+    public static void main(String[]args){
+        JSONObject object=new JSONObject();
+        object.put("name", "Neha");
+        object.put("num", new Integer(103));
+        object.put("balance", new Double(1000.21));
+        object.put("is_vip", new Boolean(true));
+        StringWriter out=new StringWriter();
+        object.writeJSONString(out);
+        String jsonText=out.toString();
+        System.out.println(jsonText);
+    }
+}
+*/
